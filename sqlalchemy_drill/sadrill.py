2012b022492e7f8173067154287a0eff089ffe04
johnomernik <john@omernik.com>
2017-02-25 06:29:53 -0600

    Debugging

--- sqlalchemy_drill/sadrill.py
+++ sqlalchemy_drill/sadrill.py
@@ -196,7 +196,8 @@ class DrillDialect_sadrill(default.DefaultDialect):
         q = "SELECT * FROM %(table_id)s LIMIT 1" % ({"table_id": table_name})
 
         print("in get columns!!!!!")
-        cursor = connection.cursor.execute(q)
+        concurs = connection.cursor()
+        cursor = concurs.execute(q)
         result = []
         #db = drill.connect(host=self.host, port=self.port)
  #       cursor = connection.cursor()
