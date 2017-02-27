24fe72f7818d8e3dbd2ee1dd8a0bca93fa2146fc
johnomernik <john@omernik.com>
2017-02-27 13:18:11 -0600

    Added some dbi api things

--- sqlalchemy_drill/sadrill.py
+++ sqlalchemy_drill/sadrill.py
@@ -166,13 +166,20 @@ class DrillDialect_sadrill(default.DefaultDialect):
             return False
 
     def get_columns(self, connection, table_name, schema=None, **kw):
-        q = "SELECT * FROM %(table_id)s LIMIT 0" % ({"table_id": table_name})#
+        q = "SELECT * FROM %(table_id)s LIMIT 1" % ({"table_id": table_name})#
 
         print("in get columns!!!!!")
 #        q = "DESCRIBE %(table_id)s" % ({"table_id": table_name})
         cursor = connection.execute(q)
 
         print("Description")
+
+        for row in cursor:
+            print(row)
+            print(type(row))
+            print(dir(row))
+
+
         result = []
         for info in cursor:
             print(type(info))
