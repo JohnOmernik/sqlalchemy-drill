b2416d64fa91f6d0c448851fe2b837b5962ab975
johnomernik <john@omernik.com>
2017-02-27 11:19:28 -0600

    Added some dbi api things

--- sqlalchemy_drill/sadrill.py
+++ sqlalchemy_drill/sadrill.py
@@ -253,7 +253,8 @@ class DrillDialect_sadrill(default.DefaultDialect):
 #        print (kw)
 
  #       drill = PyDrill(host=self.host, port=self.port)
-        file_dict = connection.execute("SHOW FILES IN " + location)
+        #file_dict = connection.execute("SHOW FILES IN " + location)
+        file_dict = connection.execute("SHOW FILES")
 
         temp = []
         for row in file_dict:
