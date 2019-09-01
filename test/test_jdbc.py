c7e29dad3a1348bde22413fda03134f99069d6d3
Charles Givre <cgivre@apache.org>
2019-09-01 15:47:01 -0400

    Fixed Hard Coded Paths and Added License Headers

--- test/test_jdbc.py
+++ test/test_jdbc.py
@@ -1,7 +1,25 @@
-import sqlalchemy
+# This is the MIT license: http://www.opensource.org/licenses/mit-license.php
+#
+# Copyright (c) 2005-2012 the SQLAlchemy authors and contributors <see AUTHORS file>.
+# SQLAlchemy is a trademark of Michael Bayer.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+# to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
 from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-from sqlalchemy import inspect
 
 engine = create_engine('drill+sadrill://localhost:8047/dfs?use_ssl=False')
 
@@ -9,7 +27,6 @@ with engine.connect() as con:
     rs = con.execute('SELECT * FROM cp.`employee.json` LIMIT 5')
     for row in rs:
         print(row)
-
 
 print("Now JDBC")
 jdbc_engine = create_engine('drill+jdbc://admin:password@localhost:31010')
