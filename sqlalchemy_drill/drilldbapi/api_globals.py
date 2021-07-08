e67dde14b4efe1f425ad1dd1783f1145d2b37f1b
dzamo <9107319+dzamo@users.noreply.github.com>
2021-07-08 16:51:49 +0200

    Rewrite the Drill DB-API implementation using ijson. (#69)

--- sqlalchemy_drill/drilldbapi/api_globals.py
+++ sqlalchemy_drill/drilldbapi/api_globals.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 # Global variables
-_HEADER = {"Content-Type": "application/json"}
-_PAYLOAD = {"queryType": "SQL", "query": None}
-_LOGIN = {"j_username": None, "j_password": None}
+_HEADER = {'Content-Type': 'application/json'}
+_PAYLOAD = {'queryType': 'SQL', 'query': None}
+_LOGIN = {'j_username': None, 'j_password': None}
+_PROGRESS_LOG_N = 10_000
+
