e891a75c8191c37411608506dcab24bec56e0321
James Turton <james@somecomputer.xyz>
2024-06-04 14:25:11 +0200

    Bump version to 1.1.5.

--- sqlalchemy_drill/__init__.py
+++ sqlalchemy_drill/__init__.py
@@ -19,7 +19,7 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-__version__ = '1.1.3'
+__version__ = '1.1.5'
 from sqlalchemy.dialects import registry
 
 registry.register("drill", "sqlalchemy_drill.sadrill", "DrillDialect_sadrill")
