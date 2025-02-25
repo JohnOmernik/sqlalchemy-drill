fa5c137cdbc2b83008e0d9bbc5c3985030c1e013
Jarek Potiuk <jarek@potiuk.com>
2025-02-25 21:05:38 +0100

    Fix wrongly delegated import_dbapi method
    The import_dbapi method should be called on DrillDialect_sadrill
    class not the compiler one.
    
    Fixes: #92

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with io.open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='sqlalchemy_drill',
-      version='1.1.6',
+      version='1.1.7',
       description="Apache Drill for SQLAlchemy",
       long_description=long_description,
       long_description_content_type="text/markdown",
